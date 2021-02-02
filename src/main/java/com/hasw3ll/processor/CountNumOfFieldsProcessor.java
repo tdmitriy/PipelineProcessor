22e0687708edf7296297eeee7a19421f6d6d276f
hasw3ll <haswelldt@gmail.com>
2021-02-02 16:38:43 +0200

    refactor some code

--- src/main/java/com/hasw3ll/processor/CountNumOfFieldsProcessor.java
+++ src/main/java/com/hasw3ll/processor/CountNumOfFieldsProcessor.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 public class CountNumOfFieldsProcessor implements Processor {
     private Map<String, String> configuration;
-    public static final String COUNT_FIELD_NAME = "numOfFields";
 
     @Override
     public void initialize(Map<String, String> configuration) {
@@ -14,8 +13,7 @@ public class CountNumOfFieldsProcessor implements Processor {
     @Override
     public void process(Map<String, Object> jsonDocument) {
         int countOfFields = jsonDocument.keySet().size();
-        jsonDocument.put(COUNT_FIELD_NAME, countOfFields);
-
+        configuration.forEach((k, v) -> jsonDocument.put(v, countOfFields));
         System.out.println("CountNumOfFieldsProcessor -- " + jsonDocument);
     }
 }
